2523e29a367e36ad45401faf5648760ba823e6fb
soksereyvisalBIU <soksereyvisal4141@gmail.com>
2024-07-03 17:18:24 +0700

    Update web.php

--- library/routes/web.php
+++ library/routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/test', function () {
     return view('client.book.index');
 });
